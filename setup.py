d691506a4c3a170d303aa363fa82f7e2c2ba6f2d
jamesbeedy <jamesbeedy@gmail.com>
2020-07-01 13:02:22 -0700

    rename to slurmd-peer

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='slurmd-inventory',
-    packages=find_packages(include=['slurmd_inventory']),
+    name='slurmd-peer',
+    packages=find_packages(include=['slurmd_peer']),
     version='0.0.1',
     license='MIT',
     long_description=open('README.md', 'r').read(),
-    url='https://github.com/omnivector-solutions/interface-slurmd-inventory',
+    url='https://github.com/omnivector-solutions/interface-slurmd-peer',
     install_requires=[],
     python_requires='>=3.6',
 )
